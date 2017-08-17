dc006606ce9748b657f29b6e443fb65a202e7f49
Hamza Hajji <hmzhjji@gmail.com>
2017-08-17 20:22:52 +0100

    add click handler and refactor code

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 const API_KEY = 'FBVLnLhjifmshnoctoI9vwM8zF6Xp1izu7KjsnZPNpE95HKLWL';
 
-
-$.ajax({
+function fetchQuote(callback) {
+  $.ajax({
     url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=10"', // The URL to the API. You can get this in the API page of the API you intend to consume
     type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
     dataType: 'json',
-    success: function(data) {
-      $('.quote').html(data.quote);
-    },
+    success: callback,
     beforeSend: function(xhr) {
       xhr.setRequestHeader("X-Mashape-Authorization", API_KEY); // Enter here your Mashape key
     }
+  });
+}
+
+$(document).ready(function () {
+  $quote = $('.quote');
+  $author = $('.author');
+  fetchQuote(function (data) {
+    $quote.text(data.quote);
+    $author.html("&mdash;&nbsp;&nbsp;" + data.author);
+  });
+  $button = $('button.refresh');
+  $button.click(function (e) {
+    fetchQuote(function (data) {
+      console.log(data);
+      $quote.text(data.quote);
+      $author.html("&mdash;&nbsp;&nbsp;" + data.author);
+    });
+  });
 });
